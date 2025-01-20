469ba741fff76350f56e7e7a3755bfa97f856afe
juliomartinsss <160731695+juliomartinsss@users.noreply.github.com>
2025-01-20 11:39:03 -0300

    teste

---
+++ home/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("A view funcionou")
+
+def sobre(request):
+    return HttpResponse("<h1> Sistema 1.0 desenvolvido por mim </h1>")
+
+def contato(request):
+    return HttpResponse("<h1> esta é a página de contato</h1>")
+
+def ajuda(request):
+    return HttpResponse("<h1>Esta é  a página de ajuda </h1>")
+
+from django.shortcuts import HttpResponse
+def index(request):
+    return render (request,'index.html')
+
+def sobre(request):
+    return render (request,'sobre.html')
+
+def contato(request):
+    return render (request,'contato.html')
+
+def ajuda(request):
+    return render (request,'ajuda.html')
